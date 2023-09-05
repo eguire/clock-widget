45c106d7d7cfc00a89ce20baa9d18e0dcf8838db
Eduard Honcharov <bad.sand.king@gmail.com>
2023-09-05 17:53:24 +0300

    Fixed day names

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-	const buttonsArr = document.querySelectorAll('.clock__button'),
-				content = document.querySelector('.clock__content');
+	const buttonsArr = document.querySelectorAll('.widget__button'),
+				widgets = document.querySelectorAll('.widget__content'),
+				clock = document.querySelector('.widget__clock');
+	let clockInterval = setInterval(showTime, 1000);
 
-	buttonsArr.forEach(btn => {
+	buttonsArr.forEach((btn, i) => {
 		btn.addEventListener('click', (e) => {
-			switch (e.target.innerText) {
-				case 'Clock':
-					showTime();
-					break;
-				case 'Stopwatch':
-					console.log('stopwatch')
-					break;
-				case 'Timer':
-					console.log('timer')
-					break;
+			if (btn == e.target) {
+				removeActiveClass();
+				widgets[i].classList.add('widget-active');
 			}
 		})
 	})
 
-	function showTime() {
-		updateTime();
-		setInterval(updateTime, 1000);
+	function removeActiveClass() {
+		widgets.forEach(block => {
+			block.classList.remove('widget-active')
+		})
+	}
 
-		function updateTime() {
-			const date = new Date(),
-						month = getMonthName(date.getMonth()),
-						dayName = getDayName(date.getDay()),
-						day = date.getDate(),
-						hours = date.getHours(),
-						minutes = date.getMinutes();
-
-			content.innerHTML = `<div class="clock__hours">${hours} : ${minutes}</div><div class="clock__day">${dayName}</div><div class="clock__date">${day} ${month}</div>`
+	function getZero(num) {
+		if (num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
 		}
+	}
+
+	function showTime() {
+		const date = new Date(),
+					month = getMonthName(date.getMonth()),
+					dayName = getDayName(date.getDay()),
+					day = date.getDate(),
+					hours = getZero(date.getHours()),
+					minutes = getZero(date.getMinutes());
+
+		clock.innerHTML = `<div class="widget__hours">${hours} : ${minutes}</div><div class="widget__day">${dayName}</div><div class="widget__date">${day} ${month}</div>`;
 
 		function getDayName(num) {
 			switch (num) {
+				case 0:
+					return 'Sunday';
 				case 1:
 					return 'Monday';
-				case 2: 
+				case 2:
 					return 'Tuesday';
-				case 3: 
+				case 3:
 					return 'Wednesday';
-				case 4: 
+				case 4:
 					return 'Thursday';
-				case 5: 
+				case 5:
 					return 'Friday';
-				case 6: 
+				case 6:
 					return 'Saturday';
-				case 7: 
-					return 'Sunday';
 			}
 		}
 
@@ -57,32 +61,33 @@ window.addEventListener('DOMContentLoaded', () => {
 			switch (num) {
 				case 0:
 					return 'January';
-				case 1: 
+				case 1:
 					return 'February';
-				case 2: 
+				case 2:
 					return 'March';
-				case 3: 
+				case 3:
 					return 'April';
-				case 4: 
+				case 4:
 					return 'May';
-				case 5: 
+				case 5:
 					return 'June';
-				case 6: 
+				case 6:
 					return 'July';
-				case 7: 
+				case 7:
 					return 'August';
-				case 8: 
+				case 8:
 					return 'September';
-				case 9: 
+				case 9:
 					return 'October';
-				case 10: 
+				case 10:
 					return 'November';
-				case 11: 
+				case 11:
 					return 'December';
 			}
 		}
 	}
 
 	buttonsArr[0].click();
+	showTime();
 
 })
