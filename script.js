79955e40a8cdaa13ba1407f3e7fb8917e5b4977c
Eduard Honcharov <bad.sand.king@gmail.com>
2023-09-14 19:54:39 +0300

    Created logic for stopwatch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
 
 	const buttonsArr = document.querySelectorAll('.widget__button'),
-				widgets = document.querySelectorAll('.widget__content'),
-				clock = document.querySelector('.widget__clock');
+		widgets = document.querySelectorAll('.widget__content'),
+		clock = document.querySelector('.widget__clock');
 	let clockInterval = setInterval(showTime, 1000);
 
 	buttonsArr.forEach((btn, i) => {
@@ -30,11 +30,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function showTime() {
 		const date = new Date(),
-					month = getMonthName(date.getMonth()),
-					dayName = getDayName(date.getDay()),
-					day = date.getDate(),
-					hours = getZero(date.getHours()),
-					minutes = getZero(date.getMinutes());
+			month = getMonthName(date.getMonth()),
+			dayName = getDayName(date.getDay()),
+			day = date.getDate(),
+			hours = getZero(date.getHours()),
+			minutes = getZero(date.getMinutes());
 
 		clock.innerHTML = `<div class="widget__hours">${hours} : ${minutes}</div><div class="widget__day">${dayName}</div><div class="widget__date">${day} ${month}</div>`;
 
@@ -87,7 +87,62 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	buttonsArr[0].click();
+	function stopWatch() {
+		const display = document.querySelector('.widget__sw-display'),
+					btnsWrapper = document.querySelector('.widget__sw-buttons');
+		let interval,
+				startTime,
+				elapsedTime,
+				time = {milliseconds: 0, tens: 0, seconds: 0, minutes: 0, hours: 0};
+
+		btnsWrapper.addEventListener('click', (e) => {
+			const target = e.target;
+
+			if (target.classList.contains('widget__sw-btn--start')) { // Start stopwatch
+				target.classList.replace('widget__sw-btn--start', 'widget__sw-btn--pause'); // Change start button
+				target.textContent = 'Pause';
+
+				document.querySelector('.widget__sw-btn--lap').classList.add('widget__sw-btn--active'); // Change lap button
+				document.querySelector('.widget__sw-btn--reset').classList.remove('widget__sw-btn--active'); // Change reset btn
+				
+				startTime = startTime ? Date.now() - elapsedTime : Date.now(); // Determine start time
+				interval = setInterval(updateDisplay, 10);
+			} else if (target.classList.contains('widget__sw-btn--pause')) { // Pause
+				target.classList.replace('widget__sw-btn--pause', 'widget__sw-btn--start'); // Change start button
+				target.textContent = 'Start';
+				
+				document.querySelector('.widget__sw-btn--lap').classList.remove('widget__sw-btn--active');
+				document.querySelector('.widget__sw-btn--reset').classList.add('widget__sw-btn--active');
+
+				clearInterval(interval)
+			} else if (target.classList.contains('widget__sw-btn--reset') && target.classList.contains('widget__sw-btn--active')) { // Reset
+				target.classList.remove('widget__sw-btn--active');
+
+				for (key in time) {
+					time[key] = 0;
+				}
+
+				startTime = 0;
+				elapsedTime = 0;
+
+				display.textContent = '00:00.00';
+			}
+		})
+
+		function updateDisplay() {
+			elapsedTime = Date.now() - startTime;
+			time.milliseconds = elapsedTime % 1000;
+			time.tens = Math.floor(time.milliseconds / 10);
+			time.seconds = Math.floor((elapsedTime / 1000) % 60);
+			time.minutes = Math.floor((elapsedTime / 1000 / 60) % 60);
+			time.hours = Math.floor(elapsedTime / 1000 / 60 / 60);
+
+			display.textContent = `${getZero(time.minutes)}:${getZero(time.seconds)}.${getZero(time.tens)}`;
+		}
+	}
+
 	showTime();
+	buttonsArr[0].click();
+	stopWatch();
 
 })
